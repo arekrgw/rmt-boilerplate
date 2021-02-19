421b15848f63922c8458c2cd0b8d7d716891cf7a
arekrgw <arkadiusz.pawlak.web@gmail.com>
2021-02-19 22:19:39 +0100

    qs3

--- src/api/queryHooks.ts
+++ src/api/queryHooks.ts
@@ -2,5 +2,5 @@ import { useQuery } from 'react-query';
 import Queries from '@api/queries';
 
 export const usePosts = () => {
-  return useQuery(...Queries.postsQuery())
+  return useQuery(...Queries.postsQuery());
 };
